502a0dbfdc59d78c22c1baffec53f0b5b0d330ce
JOSEPDAM\infci <jprodrigogarcia01.26@gmail.com>
2025-02-14 09:49:04 +0100

    Comando start

--- esqueleto.py
+++ esqueleto.py
@@ -18,7 +18,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
     await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
+        rf"¡Bienvenid@ {user.mention_html()}! Usa el comando /love para empezar a encontrar el amor",
         reply_markup=ForceReply(selective=True),
     )
 
