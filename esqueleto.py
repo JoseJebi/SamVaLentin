728ae165026ae32b7309af728b4f2067ef0106c9
JOSEPDAM\infci <jprodrigogarcia01.26@gmail.com>
2025-02-14 09:42:08 +0100

    inicial 2

---
+++ esqueleto.py
@@ -0,0 +1,35 @@
+from telegram import Update, ReplyKeyboardMarkup, ForceReply
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext, ContextTypes
+from diccionarioAUtilizar import personas
+import logging
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
+TOKEN = "8085222960:AAH6TDXo5-vYTniLrrbbh_VDIKAHG9fdL_w"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Hi {user.mention_html()}!",
+        reply_markup=ForceReply(selective=True),
+    )
+
+def main():
+    """Configura y ejecuta el bot."""
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
